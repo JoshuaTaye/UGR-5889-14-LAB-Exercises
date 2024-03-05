1dcb1c57257808613c90f742d6a8d4a0dd3d98a9
JoshuaTaye <101046138+JoshuaTaye@users.noreply.github.com>
2024-03-05 19:57:13 +0300

    Update LAB-1-Classes-and-objects-exercise-2.dart

--- LAB-1-Classes-and-objects-exercise-2.dart
+++ LAB-1-Classes-and-objects-exercise-2.dart
@@ -1,1 +1,17 @@
+class Car {
+  String brand;
+  String model;
+  int year;
+  Car(this.brand, this.model, this.year);
+  void vroom(){
+    print('Vroom Vroom');
+  }
+}
 
+void main() {
+  Car Car1 = Car('Toyota', 'Camry', 2022);
+  print('Brand: ${Car1.brand}');
+  print('Model: ${Car1.model}');
+  print('Year: ${Car1.year}');
+  Car1.vroom();
+}
