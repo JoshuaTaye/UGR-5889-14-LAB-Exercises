cd128451f791150e035c1b1bc1e2e0600f2a8263
JoshuaTaye <101046138+JoshuaTaye@users.noreply.github.com>
2024-03-05 20:33:51 +0300

    Update LAB-1-Exception-Handling-exercise-2.dart

--- LAB-1-Exception-Handling-exercise-2.dart
+++ LAB-1-Exception-Handling-exercise-2.dart
@@ -1,1 +1,20 @@
+void divideNumbers(int a, int b) {
+  if (b == 0) {
+    throw ArgumentError('Cannot divide by zero');
+  }
 
+  int result = a ~/ b;
+  print('Result: $result');
+}
+
+void main() {
+  try {
+    divideNumbers(10, 0);
+  } catch (e) {
+    if (e is ArgumentError) {
+      print('ArgumentError occurred: $e');
+    } else {
+      print('An exception occurred: $e');
+    }
+  }
+}
