c352856434c73da9a7e5df6206935b537dbb419f
JoshuaTaye <101046138+JoshuaTaye@users.noreply.github.com>
2024-03-05 20:32:34 +0300

    Update LAB-1-Exception-Handling-exercise-1.dart

--- LAB-1-Exception-Handling-exercise-1.dart
+++ LAB-1-Exception-Handling-exercise-1.dart
@@ -1,1 +1,20 @@
+int max = 0;
+int Maximum(List a) {
+  if (a.length == 0 ) {
+    throw Exception('List is Empty');
+  }
+  for (int i = 0; i < a.length; i++) {
+      if (a[i] > max) {
+        max = a[i];
+      }
+    }
+    return max;
+}
 
+void main() {
+  try {
+    Maximum([]);
+  } catch (e) {
+    print('An exception occurred: $e');
+  }
+}
