6d583f54751e1894034e6f5e9fe6ecea4603cd02
JoshuaTaye <101046138+JoshuaTaye@users.noreply.github.com>
2024-03-05 16:52:31 +0300

    Update LAB-1-Control-flow-statements-exercise-1.dart

--- LAB-1-Control-flow-statements-exercise-1.dart
+++ LAB-1-Control-flow-statements-exercise-1.dart
@@ -1,1 +1,28 @@
-
+var day = 1;
+void main(){
+switch (day){
+  case 1:
+    print("Monday");
+    break;
+  case 2:
+    print("Tuesday");
+    break;
+  case 3:
+    print("Wednesday");
+    break;
+  case 4:
+    print("Thursday");
+    break;
+  case 5:
+    print("Friday");
+    break;
+  case 6:
+    print("Saturday");
+    break;
+  case 7:
+    print("Sunday");
+    break;
+  default:
+    print("Not a valid date");
+}
+}
