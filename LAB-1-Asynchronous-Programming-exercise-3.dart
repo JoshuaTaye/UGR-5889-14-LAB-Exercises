e9a474b0539206719a1f7ee53b347c6b53d1cbfd
JoshuaTaye <101046138+JoshuaTaye@users.noreply.github.com>
2024-03-05 20:16:31 +0300

    Update LAB-1-Asynchronous-Programming-exercise-3.dart

--- LAB-1-Asynchronous-Programming-exercise-3.dart
+++ LAB-1-Asynchronous-Programming-exercise-3.dart
@@ -1,1 +1,8 @@
+Future <int> add(int a, int b) async {
+  await Future.delayed(Duration(seconds: 3));
+  return(a + b);
+}
+void main() async{
+  await (add(3,4)).then((value) => print("Their sum is $value"));
+}
 
