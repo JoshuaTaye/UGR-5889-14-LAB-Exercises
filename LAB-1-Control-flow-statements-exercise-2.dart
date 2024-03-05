5d0baf2817681f1520ac81dfb5b031a3b051fe3b
JoshuaTaye <101046138+JoshuaTaye@users.noreply.github.com>
2024-03-05 17:29:22 +0300

    Update LAB-1-Control-flow-statements-exercise-2.dart

--- LAB-1-Control-flow-statements-exercise-2.dart
+++ LAB-1-Control-flow-statements-exercise-2.dart
@@ -1,1 +1,16 @@
+import 'dart:ffi';
+var list = [];
+// Array lst = [];
+void main(){
+
+  for (var i = 0; i < 10; i++) {
+    if(i < 2){
+      list.add(i);
+      print(list[i]);
+      continue;
+    }
+    list.add(list[i-1] + list[i-2]);
+    print(list[i]);
+  }
+}
 
