23695ec1b2533b1f4cc1f07ca0e3caed21de4fff
JoshuaTaye <101046138+JoshuaTaye@users.noreply.github.com>
2024-03-05 19:12:27 +0300

    Update LAB-1-Functions-exercise-1.dart

--- LAB-1-Functions-exercise-1.dart
+++ LAB-1-Functions-exercise-1.dart
@@ -1,1 +1,11 @@
-
+void main() {
+  num average(List lst) {
+    num sum = 0;
+    for (int i = 0; i < lst.length; i++) {
+      sum = sum + lst[i];
+    }
+    num avg = sum / (lst.length);
+    return avg;
+  }
+  print(average([1,2,3,4,5]));
+}
