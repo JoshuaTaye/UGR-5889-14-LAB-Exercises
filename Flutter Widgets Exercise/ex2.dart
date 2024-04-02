b69d2648f4d503b91874334b7293c024574ce2d1
JoshuaTaye <tayejoshua4@gmail.com>
2024-04-02 16:56:24 +0300

    Update ex2.dart

--- Flutter Widgets Exercise/ex2.dart
+++ Flutter Widgets Exercise/ex2.dart
@@ -20,11 +20,14 @@ class MyApp extends StatelessWidget {
               ,)
         ),
         body:
-           const Padding(padding: const EdgeInsets.fromLTRB(25, 0, 25, 0),
+            Padding(padding: const EdgeInsets.fromLTRB(25, 0, 25, 0),
                 child:
                 Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                     Row(
+                    Column(
+                      children: [
+                     const Row(
                       children: [
                          IconButton(icon: Icon(Icons.shopping_cart, color: Colors.indigo,), onPressed: null),
                         Column(
@@ -36,16 +39,20 @@ class MyApp extends StatelessWidget {
                         )
                       ],
                     ),
-                     Column(
+                     const Column(
                       children: [
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
-                            Text("Image Placeholder"),
+                            Image(width:100,
+                                height: 100,
+                                image: AssetImage('./assets/angel_hair.jpeg')
+                            ),
                             Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Text("Calas"),
-                                Text("15.00", style:TextStyle(fontSize: 20))
+                                Text("\$15.00", style:TextStyle(fontSize: 20))
                               ],
                             ),
                             Column(
@@ -57,6 +64,65 @@ class MyApp extends StatelessWidget {
                             ),
                           ],
                         ),
+                         Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                             Image(width:100,
+                                height: 100,
+                                image: AssetImage('./assets/calas.jpeg')
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Angel Hair"),
+                                Text("Salmon, Mozzarella"),
+                                Text("\$22.99", style:TextStyle(fontSize: 20))
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                IconButton(icon: Icon(Icons.add, color: Colors.indigo,), onPressed: null),
+                                Text("1.0"),
+                                IconButton(icon: Icon(Icons.minimize, color: Colors.indigo,), onPressed: null),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                     ),
+                        Column(
+                          children: [
+                            const Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text("Subtotal"),
+                              Text("\$60.98")
+                              ],
+                            ),
+                            const Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text("TAX (10.0%)"),
+                                Text("\$6.10")
+                              ],
+                            ),
+                            Padding(padding: const EdgeInsets.fromLTRB(0,30,0,10),
+                            child:
+                            Container(
+                              padding: const EdgeInsets.all(10),
+                              color: Colors.deepOrange,
+                              child: const Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text("Checkout", style: TextStyle(color: Colors.white, fontSize: 20),),
+                                  Text("\67.08", style: TextStyle(color: Colors.white))
+                                ],
+                              )
+                            ),
+    ),
+                          ],
+                        ),
+
                       ],
                     ),
                   ],),
