c9cb879bbf5ee7aad1c4935242d40c0db6126844
JoshuaTaye <tayejoshua4@gmail.com>
2024-04-22 16:18:21 +0300

    feat: used bloc to control states

--- bloc_counter/lib/main.dart
+++ bloc_counter/lib/main.dart
@@ -1,51 +1,79 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
-abstract class CounterEvent {}
-class CounterIncrementPressed extends CounterEvent{
-
+class CounterStates{}
+class CounterEvent {}
+class InitialState extends CounterStates{}
+class UpdateState extends CounterStates {
+  final int counter;
+  UpdateState(this.counter);
 }
-class CounterDecrementPressed extends CounterEvent{
+class NumberIncreaseEvent extends CounterEvent{}
 
+class NumberDecreaseEvent extends CounterEvent{}
+
+class CounterBloc extends Bloc<CounterEvent, CounterStates> {
+  int counter = 0;
+  CounterBloc() : super(InitialState()) {
+    on<NumberIncreaseEvent>(onNumberIncrease);
+    on<NumberDecreaseEvent>(onNumberDecrease);
+  }
+
+  void onNumberIncrease(
+      NumberIncreaseEvent event, Emitter<CounterStates> emit) async {
+    counter = counter + 1;
+    emit(UpdateState(counter));
+  }
+
+  void onNumberDecrease(
+      NumberDecreaseEvent event, Emitter<CounterStates> emit) async {
+    counter = counter - 1;
+    emit(UpdateState(counter));
+  }
 }
-
-
-class CounterBloc extends Bloc<CounterEvent, int>{
-  CounterBloc(): super(0);
-  void increment() => emit(state + 1);
-  void decrement() => emit(state - 1);
-);
-
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-   return BlocProvider(
-      create: (context) => CounterBloc(),
-      child: MaterialApp(
+    return BlocProvider<CounterBloc>(
+        create: (context) => CounterBloc(),
+        child: MaterialApp(
           title: 'Flutter Demo',
           theme: ThemeData(
-            primarySwatch: Colors.blue,
+            // This is the theme of your application.
+            //
+            // TRY THIS: Try running your application with "flutter run". You'll see
+            // the application has a purple toolbar. Then, without quitting the app,
+            // try changing the seedColor in the colorScheme below to Colors.green
+            // and then invoke "hot reload" (save your changes or press the "hot
+            // reload" button in a Flutter-supported IDE, or press "r" if you used
+            // the command line to start the app).
+            //
+            // Notice that the counter didn't reset back to zero; the application
+            // state is not lost during the reload. To reset the state, use hot
+            // restart instead.
+            //
+            // This works for code too, not just values: Most code changes can be
+            // tested with just a hot reload.
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
           ),
-       home: Center(
+          home: const MyHomePage(),
 
-       ),
-      ),
-   );
+        )
+    );
+
+
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -56,25 +84,7 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -92,42 +102,66 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Counter App with Bloc'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: BlocBuilder<CounterBloc, CounterStates>(
+        builder: (context, state) {
+          if (state is InitialState) {
+            return _counter(context, 0);
+          }
+          if (state is UpdateState) {
+            return _counter(context, state.counter);
+          }
+          return Container();
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+Widget _counter(BuildContext context, int counter) {
+  return Center(
+    // Center is a layout widget. It takes a single child and positions it
+    // in the middle of the parent.
+    child: Column(
+      // Column is also a layout widget. It takes a list of children and
+      // arranges them vertically. By default, it sizes itself to fit its
+      // children horizontally, and tries to be as tall as its parent.
+      //
+      // Column has various properties to control how it sizes itself and
+      // how it positions its children. Here we use mainAxisAlignment to
+      // center the children vertically; the main axis here is the vertical
+      // axis because Columns are vertical (the cross axis would be
+      // horizontal).
+      //
+      // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+      // action in the IDE, or press "p" in the console), to see the
+      // wireframe for each widget.
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        const Text(
+          'You have pushed the button this many times:',
+        ),
+        Text(
+          counter.toString(),
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+
+        ElevatedButton(onPressed: () {
+          context.read<CounterBloc>().add(NumberDecreaseEvent());
+        },
+            child: const Text('-')),
+
+        ElevatedButton(onPressed: () {
+          context.read<CounterBloc>().add(NumberIncreaseEvent());
+        },
+            child: const Text('+')),
+      ],
+    ),
+
+    // floatingActionButton: FloatingActionButton(
+    //   onPressed: _incrementCounter,
+    //   tooltip: 'Increment',
+    //   child: const Icon(Icons.add),
+    // ), // This trailing comma makes auto-formatting nicer for build methods.
+  );
 }
