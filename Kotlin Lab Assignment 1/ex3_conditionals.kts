9c8129b3bd77209af914d4f50367b0812a128156
JoshuaTaye <101046138+JoshuaTaye@users.noreply.github.com>
2024-05-13 15:31:13 +0300

    Update ex3_conditionals.kts

--- Kotlin Lab Assignment 1/ex3_conditionals.kts
+++ Kotlin Lab Assignment 1/ex3_conditionals.kts
@@ -1,1 +1,20 @@
-
+println("Enter your score:")
+val g:Int? = readLine()?.toIntOrNull()
+    if (g == null) {
+        println("Invalid input for the grade")
+    }
+    else if (g >= 90){
+        println("A")
+    }
+    else if (g >= 80){
+        println("B")
+    }
+    else if (g >= 70){
+        println("C")
+    }
+    else if (g >= 60){
+        println("D")
+    }
+    else {
+        println("F")
+    }
