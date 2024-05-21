386cb3d79f13f4296f6b05f9f29386c8efe4fba8
JoshuaTaye <tayejoshua4@gmail.com>
2024-05-21 16:03:26 +0300

    Kotlin app

--- KotlinCounterApp/app/src/main/java/com/example/counterapp/MainActivity.kt
+++ KotlinCounterApp/app/src/main/java/com/example/counterapp/MainActivity.kt
@@ -1,30 +1,24 @@
 package com.example.counterapp
+
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-
 class MainActivity : AppCompatActivity() {
-
     private lateinit var countTextView: TextView
     private lateinit var incrementButton: Button
     private lateinit var decrementButton: Button
     private lateinit var resetButton: Button
-
     private var count = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         countTextView = findViewById(R.id.countTextView)
         incrementButton = findViewById(R.id.incrementButton)
         decrementButton = findViewById(R.id.decrementButton)
         resetButton = findViewById(R.id.resetButton)
-
         countTextView.text = count.toString()
-
         incrementButton.setOnClickListener { incrementCount() }
         decrementButton.setOnClickListener { decrementCount() }
         resetButton.setOnClickListener { resetCount() }
@@ -36,7 +30,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun decrementCount() {
-        count--
+        if (count > 0) {
+            count--
+        }
         countTextView.text = count.toString()
     }
 
